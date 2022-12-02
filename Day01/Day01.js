8815683df9fd6b65ceb2721c67cf8fb15b6937c9
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-12-02 11:01:28 +0100

    ref

--- Day01/Day01.js
+++ Day01/Day01.js
@@ -1,43 +1,18 @@
 import { readTXTContent } from "../readFile.js";
-
-function day01_part1() {
-  let content = readTXTContent("input.txt").replace(/[\r]/g, "");
-  let contentAsArray = content.split("\n");
-  let currentRecord = 0;
-  let currStreak = 0;
-  contentAsArray.map((el) =>
-    el == ""
-      ? currStreak > currentRecord
-        ? ((currentRecord = currStreak), (currStreak = 0))
-        : (currStreak = 0)
-      : (currStreak += parseInt(el))
-  );
-  console.log(currentRecord);
-}
-
-function day01_part2() {
-  let content = readTXTContent("input.txt").replace(/[\r]/g, "");
-  let contentAsArray = content.split("\n");
-  let currentRecord = 0;
-  let currStreak = 0;
-  let groupedCaloried = [];
-  contentAsArray.map((el) => {
-    if (el == "") {
-      if (currStreak > currentRecord) {
-        currentRecord = currStreak;
-      }
-      groupedCaloried.push(currStreak);
-      currStreak = 0;
-    } else {
-      currStreak += parseInt(el);
-    }
-  });
-  console.log(
-    groupedCaloried
-      .sort((a, b) => a - b)
-      .slice(-3)
-      .reduce((partialSum, a) => partialSum + a, 0)
-  );
-}
-day01_part1();
-day01_part2();
+console.log(
+  readTXTContent("input.txt")
+    .replace(/[\r]/g, "")
+    .split("\n\n")
+    .map((line) => line.split("\n").reduce((a, b) => parseInt(a) + parseInt(b)))
+    .sort((a, b) => a - b)
+    .slice(-1)[0]
+);
+console.log(
+  readTXTContent("input.txt")
+    .replace(/[\r]/g, "")
+    .split("\n\n")
+    .map((line) => line.split("\n").reduce((a, b) => parseInt(a) + parseInt(b)))
+    .sort((a, b) => a - b)
+    .slice(-3)
+    .reduce((a, b) => parseInt(a) + parseInt(b))
+);
